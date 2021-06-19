100dfccd95668dfdc3b27f634a1829cfd1442140
ibnu-aji <ibuajipangestu@gmail.com>
2021-06-19 20:11:55 +0700

    Upload pertama

---
+++ layout/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistem Informasi Akademik UIN Sumatera Utara Medan</title>
+    <link rel="stylesheet" href="<?php echo ASSET; ?>css/style.css">
+</head>
+<body>
+    <main>
+    <header>
+        <img src="<?php echo ASSET; ?>image/c.png" alt="IMG" >
+        
+    </header>
+    <nav>
+        <ul>
+            <li>
+                <a href="index.php"class="active">Home</a> 
+            </li>
+            <li>
+                <a href="index.php?page=login_form ">login</a> 
+            </li>
+          
+        </ul>
+    </nav>
+    <section>
+    <?php  
+    if (isset($_GET['page'])) {
+            include $_GET['page'] . ".php";
+        } else {
+            include "main_index.php ";
+        }
+        ?>
+    </section>
+    
+    <footer>
+        Copyright &copy; 2021 Ibnu AJi Pangestu
+    </footer>
+      
+   
+    </main>
+</body>
+</html>
